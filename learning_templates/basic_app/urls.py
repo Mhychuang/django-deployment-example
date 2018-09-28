7b8d325d96885e003cfd6d9e12757ef02263a213
Michael Chuang <MChuang1@sboe.state.nc.us>
2018-09-28 09:24:25 -0400

    'first'

--- learning_templates/basic_app/urls.py
+++ learning_templates/basic_app/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
-from . import views
-
-# SET THE NAMESPACE!
+from django.urls import path, include, re_path
+from basic_app import views
+#template tagging
 app_name = 'basic_app'
-
-urlpatterns=[
-    path('relative/',views.relative,name='relative'),
-    path('other/',views.other,name='other'),
+urlpatterns =[
+    path('relative/', views.relative, name = 'relative'),
+    path('other', views.other, name = 'other'),
 ]
